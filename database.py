1c1960a078d7004ac60e1b15953f649af3e99494
zhangwilliam208 <124252258+zhangwilliam208@users.noreply.github.com>
2023-10-08 15:32:33 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -2,11 +2,22 @@ from sqlalchemy import create_engine, text
 import os
 my_secret = os.environ['DB_CONNECTION_STRING']
 engine=create_engine(my_secret)
+
 def load_jobs_from_db():
   with engine.connect() as conn:
     result=conn.execute(text("SELECT * FROM jobs"))
     jobs=[]
-    #print(result.all()[0]._asdict())
     for row in result.all():
       jobs.append(row._asdict())
   return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs where id= :val"), {"val":id})
+    rows=result.all()
+    if len(rows)==0:
+      return None
+    else:
+     return rows[0]._asdict()
