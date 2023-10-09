5f62749c384d262bbb7ccf0a7573c43373496aca
zhangwilliam208 <124252258+zhangwilliam208@users.noreply.github.com>
2023-10-09 09:20:08 +0000

    add Insert statement

--- database.py
+++ database.py
@@ -21,3 +21,16 @@ def load_job_from_db(id):
       return None
     else:
      return rows[0]._asdict()
+
+
+def add_application_to_db(job_id,data):
+  with engine.connect() as conn:
+        query=text("INSERT INTO applications (job_id,full_name,email,linkedin_url,education,work_experience,resume_url) VALUES(:job_id,:full_name,:email,:linkedin_url,:education,:work_experience,:resume_url)")
+        conn.execute(query, {"job_id":job_id,   
+                    "full_name":data['full_name'],
+                    "email":data['email'],
+                    "linkedin_url":data['linkedin_url'],
+                    "education":data['education'],
+                    "work_experience":data['work_experience'],
+                    "resume_url":data['resume_url']})
+  engine.dispose()
